afd09405c9fdf740082c156de4cb28638a1e042b
d3oby <zelazk.o@email.cz>
2021-04-30 17:32:06 +0200

    init

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    'plugin:nuxt/recommended',
+  ],
+  plugins: [
+  ],
+  // add your custom rules here
+  rules: {
+    semi: [ 'error', 'always' ],
+    'object-curly-spacing': [ 'error', 'always' ],
+    'array-bracket-spacing': [ 'error', 'always' ],
+    'comma-dangle': [ 'error', 'always-multiline' ],
+  },
+};
