da28d9bee043279b5b37770c3773d9d18bc2708b
doooby <zelazk.o@email.cz>
2021-09-15 22:59:50 +0200

    fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,4 +1,4 @@
-const isProduction = process.env.NODE_ENV !== 'production';
+const isProduction = process.env.NODE_ENV === 'production';
 
 const serverHostName = process.env.SERVER_HOST_NAME;
 if (isProduction && !serverHostName) {
