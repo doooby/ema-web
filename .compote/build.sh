aee836d8fa8e97225c290bedfd25a5ebb20ee42f
doooby <zelazk.o@email.cz>
2021-09-21 21:10:37 +0200

    test gziped build

--- .compote/build.sh
+++ .compote/build.sh
@@ -1,18 +1,12 @@
-mount_app="-v $stack_path/src:/app"
-mount_node_modules="-v $stack_path/var/node_modules:/app/node_modules"
-mount_nuxt_build="-v $stack_path/var/nuxt:/app/.nuxt"
-
 libops_print "compiling release images"
 
-libops_print -p "updating node_modules"
+libops_print -p "install & build app"
 libops_docker_run app:base \
-    "$mount_app $mount_node_modules" \
-    "yarn install --silent"
-
-libops_print -p "building nuxt.js"
-libops_docker_run app:base \
-    "$mount_app $mount_node_modules $mount_nuxt_build" \
-    "yarn build"
+  "-v $stack_path/src:/app" \
+  "-v $stack_path/var/node_modules:/app/node_modules" \
+  "-v $stack_path/var/nuxt:/app/.nuxt" \
+  "-v $stack_path/var/build:/app/tmp/build" \
+  "bash .compote/images/app/release/build.sh"
 
 libops_print -p "building"
 bin/build_image app release
