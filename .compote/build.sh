d2b530c1f58195468bdf658a79283835ac67e5bf
doooby <zelazk.o@email.cz>
2021-09-21 21:21:45 +0200

    test gziped build

--- .compote/build.sh
+++ .compote/build.sh
@@ -2,10 +2,10 @@ libops_print "compiling release images"
 
 libops_print -p "install & build app"
 libops_docker_run app:base \
-  "-v $stack_path/src:/app" \
-  "-v $stack_path/var/node_modules:/app/node_modules" \
-  "-v $stack_path/var/nuxt:/app/.nuxt" \
-  "-v $stack_path/var/build:/app/tmp/build" \
+  "-v $stack_path/src:/app \
+    -v $stack_path/var/node_modules:/app/node_modules \
+    -v $stack_path/var/nuxt:/app/.nuxt \
+    -v $stack_path/var/build:/app/tmp/build" \
   "bash .compote/images/app/release/build.sh"
 
 libops_print -p "building"
