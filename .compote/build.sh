a220cb02e30f9162f84cc568d7cfaa5ed2f14fb4
doooby <zelazk.o@email.cz>
2021-10-03 22:44:50 +0200

    fix release

--- .compote/build.sh
+++ .compote/build.sh
@@ -11,6 +11,7 @@ libops_print -p "pack resources"
 libops_docker_run app:base \
   "-v $stack_path/src:/app \
     -v $stack_path/var/node_modules:/app/node_modules \
+    -v $stack_path/var/nuxt:/app/.nuxt \
     -v $stack_path/var/build:/app/tmp/build" \
   "bash .compote/images/app/release/pack.sh"
 
