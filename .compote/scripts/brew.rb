f819f8d11c6c8acb9cf5be6761c54132c4574aa9
doooby <zelazk.o@email.cz>
2023-08-15 10:03:28 +0200

    t147 compote ops

--- .compote/scripts/brew.rb
+++ .compote/scripts/brew.rb
@@ -2,9 +2,9 @@
 Compote.log :yellow, '===== building nuxt app'
 Compote.run <<-CMD
 sudo docker run --rm \\
-  -v src:/app \\
-  -v var/dev_node_modules:/app/node_modules \\
-  -v var/nuxt:/app/.nuxt \\
+  -v ./src:/app \\
+  -v ./var/dev_node_modules:/app/node_modules \\
+  -v ./var/nuxt:/app/.nuxt \\
   jar--#{$jar.name}:base \\
   bash .compote/images/release/build.sh
 CMD
@@ -12,10 +12,10 @@ CMD
 Compote.log :yellow, '===== packing the app'
 Compote.run <<-CMD
 sudo docker run --rm \\
-  -v src:/app \\
-  -v var/dev_node_modules:/app/node_modules \\
-  -v var/nuxt:/app/.nuxt \\
-  -v var/build:/app/tmp/build \\
+  -v ./src:/app \\
+  -v ./var/dev_node_modules:/app/node_modules \\
+  -v ./var/nuxt:/app/.nuxt \\
+  -v ./var/build:/app/tmp/build \\
   jar--#{$jar.name}:base \\
   bash .compote/images/release/pack.sh
 CMD
