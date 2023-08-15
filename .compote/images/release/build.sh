53e677e27e3582a709cdbdd72dad40b111409553
doooby <zelazk.o@email.cz>
2023-08-15 10:45:01 +0200

    t147 compote ops

--- .compote/images/release/build.sh
+++ .compote/images/release/build.sh
@@ -1,4 +1,15 @@
 set -e
 
+# $1 - dir path
+# $2 - archive path
+function archive_dir {
+  [ -f $2 ] && rm $2
+  tar -zcf $2 $1
+}
+
 NODE_ENV=development yarn install
 yarn build
+
+yarn install
+archive_dir node_modules tmp/build/node_modules.tar.gz
+archive_dir .nuxt tmp/build/nuxt.tar.gz
--- .compote/scripts/brew.rb
+++ .compote/scripts/brew.rb
@@ -5,19 +5,9 @@ sudo docker run --rm \\
   -v ./src:/app \\
   -v ./var/dev_node_modules:/app/node_modules \\
   -v ./var/nuxt:/app/.nuxt \\
-  jar--#{$jar.name}:base \\
-  bash .compote/images/release/build.sh
-CMD
-
-Compote.log :yellow, '===== packing the app'
-Compote.run <<-CMD
-sudo docker run --rm \\
-  -v ./src:/app \\
-  -v ./var/dev_node_modules:/app/node_modules \\
-  -v ./var/nuxt:/app/.nuxt \\
   -v ./var/build:/app/tmp/build \\
   jar--#{$jar.name}:base \\
-  bash .compote/images/release/pack.sh
+  bash .compote/images/release/build.sh
 CMD
 
 Compote.log :yellow, '===== building docker image'
