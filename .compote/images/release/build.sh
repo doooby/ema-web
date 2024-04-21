29a647c8911f92edac0f01c34de598ddfbcfd21f
doooby <zelazk.o@email.cz>
2024-04-21 23:54:43 +0200

    t273 debug - return yarn

--- .compote/images/release/build.sh
+++ .compote/images/release/build.sh
@@ -7,7 +7,7 @@ function archive_dir {
   tar -zcf $2 $1
 }
 
-npm install
-npm run build
+yarn install
+yarn build
 archive_dir node_modules tmp/build/node_modules.tar.gz
 archive_dir .nuxt tmp/build/nuxt.tar.gz
